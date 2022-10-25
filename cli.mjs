1bae5d0c15c2dce7ad8f2d30cfcf922e380d0b0c
agrathwohl <andrew@storyboard.fm>
2022-10-25 17:53:49 -0500

    sorta works now

--- cli.mjs
+++ cli.mjs
@@ -1,6 +1,7 @@
 import boxen from 'boxen'
 import Enquirer from 'enquirer'
 import fs from 'fs'
+import lrc2srt from './lib/lrc.js'
 
 const { prompt } = Enquirer
 
@@ -30,3 +31,25 @@ const { inputFile } = response
 const inputText = `${fs.readFileSync(inputFile)}`.trim()
 
 console.log(inputText)
+
+const response2 = await prompt({
+  type: 'input',
+  name: 'confirmLrc',
+  message: 'Does this look right? Press any key to confirm. Ctrl+C to escape.'
+})
+
+const {confirmLrc} = response2
+
+const lrcConv = lrc2srt(inputText)
+
+console.log(lrcConv)
+
+const response3 = await prompt({
+  type: 'input',
+  name: 'confirmLrcConv',
+  message: 'Does this LRC look good? If so give it a file name:'
+})
+
+fs.writeFileSync(response3.confirmLrcConv, lrcConv)
+
+console.log('DONE!')
--- index.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs')
-const lrc = require('./lib/lrc')
-
-const testLrc = fs.readFileSync('./test/test.lrc')
